a5773535eb6265714abe438a0c952b98a8597c68
Andrey dos Santos Passos <andrey@casamagalhaes.com.br>
2020-01-02 15:19:31 -0300

    Backend do projeto

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://caffeee:caffeee17k@cluster0-uj485.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//req.query = acessar query params (oara filtros)
+// req.params = acessar route params (para edição e delete)
+//req.body = acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
